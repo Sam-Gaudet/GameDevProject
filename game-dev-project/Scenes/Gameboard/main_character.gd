f697b6fcfb2a6950ebd9aaac83a5de16aabdf60c
cXLilithXc <168659834+cXLilithXc@users.noreply.github.com>
2025-04-23 10:32:31 -0400

    rigid vody for main character so it can have body entered

--- game-dev-project/Scenes/Gameboard/main_character.gd
+++ game-dev-project/Scenes/Gameboard/main_character.gd
@@ -1,21 +1,19 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 const TILE_SIZE = 64
 const GRID_SIZE = 7
 const HALF_GRID = GRID_SIZE / 2
-
-# Min and max positions based on centered grid (e.g., from -3 to +3 tiles)
+@onready var hide_timer := Timer.new()
 const MIN_POS = Vector2(-HALF_GRID, -HALF_GRID) * TILE_SIZE
 const MAX_POS = Vector2(HALF_GRID, HALF_GRID) * TILE_SIZE
-
 var is_moving = false
 var move_duration = 0.1
 var target_position: Vector2
 
 func _ready():
-	# Snap character to nearest tile position at start
 	position = position.snapped(Vector2(TILE_SIZE, TILE_SIZE))
 	target_position = position
+	hide_timer.wait_time = 5.0
 
 func _process(_delta):
 	if not is_moving:
@@ -48,3 +46,10 @@ func move_to_tile(destination: Vector2):
 
 func _on_tween_finished():
 	is_moving = false
+
+func _on_hide_timer_timeout():
+	visible = true
+
+
+func take_damaga():
+	print("take damage")
